bab8c3cb1dcc15523ae7b50e9dd31402841301cd
devincom <devin.operational@gmail.com>
2023-04-14 00:13:46 +0530

    handling card image overflow

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import '../styles/MovieCard.css'
 
 export default function MovieCard({ movie }) {
   return (
-    <div className='movie-card'>
+    <div className='movie-card card'>
         <img src={movie.Poster} alt="Image"/>
         <h2>{movie.Title}</h2>
         <p>Year : {movie.Year}</p>
